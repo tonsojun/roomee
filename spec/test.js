69377ed5f6d6e62020ef7328d835bdc474ddbc43
Anthony King <anthonykingwheels@gmail.com>
2018-07-19 17:24:58 -0700

    '(doc) Add serverSpec file'
    'server spec tests are currently failing but I do not want to change any server files until I pull the current repo'

--- spec/test.js
+++ spec/test.js
@@ -1,8 +1,8 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+describe('A Sample Test', function() {
+  describe('#a generic test', function() {
+    it('should return true when the assertion is true', function() {
+      assert.equal(1 + 1, 2);
     });
   });
 });
