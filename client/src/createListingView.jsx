b1777e6d4a782bcfe64e864b8888dd565f7bbd9b
Anthony King <anthonykingwheels@gmail.com>
2018-07-24 10:02:21 -0700

    (doc) add form to createListingView, modify css styling of elements in view

--- client/src/createListingView.jsx
+++ client/src/createListingView.jsx
@@ -1,11 +1,59 @@
 import React from 'react';
 
-const CreateListingView = () => (
-  <div>
-    <h4>
-    Create listing here
-    </h4>
-  </div>
-);
+class CreateListingView extends React.Component {
+  constructor ({ handleCreateListingButtonClick }) {
+    super({ handleCreateListingButtonClick });
+    this.state = {
+      title: '',
+      address: '',
+      city: '',
+      zipCode: '',
+      price: '',
+      descriptionTextbox: '',
+      photos: []
+    };
+    this.onChange = this.onChange.bind(this);
+    const {
+      title, state, address, city, zipCode, price, descriptionTextbox, photos
+    } = this.state;
+  }
+
+  onChange (event) {
+    const change = {};
+    change[event.target.id] = event.target.value;
+    this.setState(change);
+  }
+
+  render () {
+    return (
+      <div id="create-listing">
+        <h4>
+        Create Your Listing:
+        </h4>
+        Title:
+        <input id="title" value={this.title} onChange={this.onChange} />
+        Address:
+        <input id="address" value={this.address} onChange={this.onChange} />
+        City:
+        <input id="city" value={this.city} onChange={this.onChange} />
+        ZipCode:
+        <input id="zipCode" size="5" value={this.zipCode} onChange={this.onChange} />
+        Price:
+        <input id="price" size="4" value={this.price} onChange={this.onChange} />
+        Description:
+        <input id="descriptionTextbox" value={this.description} onChange={this.onChange} />
+        Upload Photos:   //TODO
+        <button
+          type="submit"
+          onClick={() => {
+            this.handleCreateListingButtonClick(this.state);
+          }}
+        >
+          Create Listing
+        </button>
+      </div>
+    );
+  }
+}
 
 export default CreateListingView;
