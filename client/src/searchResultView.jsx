24b8c7ddde871127b802866be4914b5a4d69c5df
Anthony King <anthonykingwheels@gmail.com>
2018-07-26 08:51:25 -0700

    Refactor: add photos to SearchResultView

--- client/src/searchResultView.jsx
+++ client/src/searchResultView.jsx
@@ -12,6 +12,21 @@ const SearchResultView = ({listing, onTitleClick}) => (
     <div>
       <h4>{listing.city}</h4>
       <h5>{listing.price}</h5>
+
+      <div>
+        {
+          listing.photos.map((photoUrl) => {
+            // cloundinary image sizes are manipulated by inserting arguments after the 'upload' part of the file parth
+            // example: "http://res.cloudinary.com/dwysumxzw/image/upload/v1532558555/kog_r_full_shot_x2ggw7.jpg";
+            // I'm not connected to the database yet and will need to test the lines below once we are
+            const arr = photoUrl.split('upload/');
+            const uploadWidth = 'upload/w_412/';
+            const resizedPhotoUrl = arr.join(uploadWidth);
+
+            <img src="`${resizedPhotoUrl}`" alt="picture of room for rent"></img>
+          })
+        }
+      </div>
     </div>
   </div>
 );
