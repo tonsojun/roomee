138e203e58d9af1ec6783f8f876643ea55ff0ba9
Kalei Trujillo <kalei096@gmail.com>
2018-07-27 13:20:31 -0700

    style format of signup view and create listing view

--- client/src/signUpView.jsx
+++ client/src/signUpView.jsx
@@ -2,30 +2,46 @@ import React from 'react';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 const SignUpView = ({onSignUp}) => (
-  <div>
-    <form id="signUp" name="signUp" method="post" action="/signUp">
-      <label htmlFor="email">
-      Email Address:
-      </label>
-      <input className="text" name="email" type="email" />
-      {' '}
-      <label htmlFor="firstname">
-        Firstname:
-      </label>
-      <input name="firstname" type="text" />
-      {' '}
-      <label htmlFor="lastname">
+  <div className="columns section level">
+    <form className="column level-item is-narrow is-offset-5" id="signUp" name="signUp" method="post" action="/signUp">
+      <div className="field">
+        <label className="label" htmlFor="email">
+        Email Address:
+        </label>
+        <div className="control">
+          <input className="input text" name="email" type="email" />
+          {' '}
+        </div>
+      </div>
+      <div className="field">
+        <label className="label" htmlFor="firstname">
+          Firstname:
+        </label>
+        <div className="control">
+          <input className="input" name="firstname" type="text" />
+          {' '}
+        </div>
+      </div>
+      <div className="field">
+      <label className="label" htmlFor="lastname">
       Lastname:
       </label>
-      <input name="lastname" type="text" />
+      <div className="control">
+      <input className="input" name="lastname" type="text" />
       {' '}
-      <label htmlFor="password">
+      </div>
+      </div>
+      <div className="field">
+      <label className="label" htmlFor="password">
       Password:
       </label>
-      <input name="password" type="password" />
+      <div className="control">
+      <input className="input" name="password" type="password" />
       {' '}
+      </div>
+      </div>
       <Link to="/loginView" style={{ textDecoration: 'none', color: '#888' }}>
-        <input onClick={onSignUp} className="btn" type="submit" value="Sign Up" />
+        <input onClick={onSignUp} className="btn button" type="submit" value="Sign Up" />
       </Link>
     </form>
   </div>
