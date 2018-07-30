50a8e31ff8fb8b3f3d6675cbd2c5cd603de0eeee
Kalei Trujillo <kalei096@gmail.com>
2018-07-30 16:49:40 -0700

    clean up of views signin login and app

--- client/src/loginView.jsx
+++ client/src/loginView.jsx
@@ -1,31 +1,41 @@
 import React from 'react';
 
-const LoginView = ({registered}) => (
-    <form id="login" name="login" method="post" action="login" className="column is-half is-offset-one-quarter">
-    {
+const LoginView = ({ registered }) => (
+  <form id="login" name="login" method="post" action="login" className="column is-half is-offset-one-quarter">
+    { // conditional rendering of sign up message
       registered ?
-      <h4>Thanks for signing up! Please log in.</h4>
-      :
-      null
+        <h4>
+        Thanks for signing up! Please log in.
+        </h4>
+        :
+        null
     }
-        <div className="field">
-            <label className="label">Email</label>
-            <div className="control has-icons-left">
-                <input className="input is-primary" type="email" name="username" placeholder="Email" />
-                <span className="icon is-small is-left"><i className="fas fa-envelope" /></span>
-            </div>
-        </div>
-        <div className="field">
-            <label className="label">Password</label>
-            <div className="control has-icons-left">
-                <input className="input" type="password" name="password" placeholder="Password" />
-                <span className="icon is-small is-left"><i className="fas fa-lock" /></span>
-            </div>
-        </div>
-        <div>
-            <input className="button is-primary" type="submit" value="Log In" />
-        </div>
-    </form>
+    <div className="field">
+      <label className="label">
+      Email
+      </label>
+      <div className="control has-icons-left">
+        <input className="input is-primary" type="email" name="username" placeholder="Email" />
+        <span className="icon is-small is-left">
+          <i className="fas fa-envelope" />
+        </span>
+      </div>
+    </div>
+    <div className="field">
+      <label className="label">
+      Password
+      </label>
+      <div className="control has-icons-left">
+        <input className="input" type="password" name="password" placeholder="Password" />
+        <span className="icon is-small is-left">
+          <i className="fas fa-lock" />
+        </span>
+      </div>
+    </div>
+    <div>
+      <input className="button is-primary" type="submit" value="Log In" />
+    </div>
+  </form>
 );
 
 export default LoginView;
