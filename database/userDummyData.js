c78c2064232d06939f61ab5c5b523c5d04cc5bd1
tonsojun <josephjun@gmail.com>
2018-08-06 10:22:34 -0700

    Adding data

---
+++ database/userDummyData.js
@@ -0,0 +1,82 @@
+var userDummyData = [
+  {
+    "name": "Boseph Bun",
+    "email": "slowcoder@yahoo.com",
+    "location": "Los Angeles, CA",
+    "cover_photo": "https://www.pexels.com/photo/defocused-image-of-lights-255379/",
+    "profile_photo": "https://www.pexels.com/photo/adult-blur-boardwalk-casual-567459/",
+    "description": "Lorem ipsum dolor sit amet, no porro tincidunt sit, malis nulla est ne. Per ad velit assentior, ex per laudem utamur dissentiet. Aliquid definiebas nec ne, ne sea congue iisque impedit. Eleifend accommodare interpretaris vix ei. Et usu simul tibique sententiae, antiopam euripidis constituto per et, quo sint denique accusamus ex."
+  },
+  {
+    "name": "Eric Night",
+    "email": "nightanddai@yahoo.com",
+    "location": "Washington DC",
+    "cover_photo": "https://www.pexels.com/photo/purple-abstract-blur-bokeh-1755/",
+    "profile_photo": "https://www.pexels.com/photo/wood-man-winter-portrait-21278/",
+    "description": "Lorem ipsum dolor sit amet, no porro tincidunt sit, malis nulla est ne. Per ad velit assentior, ex per laudem utamur dissentiet. Aliquid definiebas nec ne, ne sea congue iisque impedit. Eleifend accommodare interpretaris vix ei. Et usu simul tibique sententiae, antiopam euripidis constituto per et, quo sint denique accusamus ex."
+  },
+  {
+    "name": "Wyclef Jean Fils",
+    "email": "gonetilnovember@yahoo.com",
+    "location": "New York, NY",
+    "cover_photo": "https://www.pexels.com/photo/blur-river-blurred-background-1445/",
+    "profile_photo": "https://www.pexels.com/photo/press-conference-interview-man-person-32976/",
+    "description": "Lorem ipsum dolor sit amet, no porro tincidunt sit, malis nulla est ne. Per ad velit assentior, ex per laudem utamur dissentiet. Aliquid definiebas nec ne, ne sea congue iisque impedit. Eleifend accommodare interpretaris vix ei. Et usu simul tibique sententiae, antiopam euripidis constituto per et, quo sint denique accusamus ex."
+  },
+  {
+    "name": "Henry Hoff",
+    "email": "donteatmypotato@yahoo.com",
+    "location": "San Franciso, CA",
+    "cover_photo": "",
+    "profile_photo": "https://www.pexels.com/photo/businessman-fashion-men-shirt-98045/",
+    "description": "Lorem ipsum dolor sit amet, no porro tincidunt sit, malis nulla est ne. Per ad velit assentior, ex per laudem utamur dissentiet. Aliquid definiebas nec ne, ne sea congue iisque impedit. Eleifend accommodare interpretaris vix ei. Et usu simul tibique sententiae, antiopam euripidis constituto per et, quo sint denique accusamus ex."
+  },
+  {
+    "name": "Johnny Appleseed",
+    "email": "applepie@yahoo.com",
+    "location": "Chicago, IL",
+    "cover_photo": "https://www.pexels.com/photo/green-grass-during-daytime-in-focus-photography-167570/",
+    "profile_photo": "https://www.pexels.com/photo/adult-african-ethnicity-bow-tie-cap-173295/",
+    "description": "Lorem ipsum dolor sit amet, no porro tincidunt sit, malis nulla est ne. Per ad velit assentior, ex per laudem utamur dissentiet. Aliquid definiebas nec ne, ne sea congue iisque impedit. Eleifend accommodare interpretaris vix ei. Et usu simul tibique sententiae, antiopam euripidis constituto per et, quo sint denique accusamus ex."
+  },
+  {
+    "name": "Lebronda James",
+    "email": "nottheking@yahoo.com",
+    "location": "Los Angeles, CA",
+    "cover_photo": "https://www.pexels.com/photo/architecture-beautiful-bloom-blooming-130851/",
+    "profile_photo": "https://www.pexels.com/photo/adult-black-and-white-blur-close-up-433411/",
+    "description": "Lorem ipsum dolor sit amet, no porro tincidunt sit, malis nulla est ne. Per ad velit assentior, ex per laudem utamur dissentiet. Aliquid definiebas nec ne, ne sea congue iisque impedit. Eleifend accommodare interpretaris vix ei. Et usu simul tibique sententiae, antiopam euripidis constituto per et, quo sint denique accusamus ex."
+  },
+  {
+    "name": "Carl Junior",
+    "email": "5thbestfastfoodchain@yahoo.com",
+    "location": "Dallas, TX",
+    "cover_photo": "https://www.pexels.com/photo/red-wooden-lounge-chair-on-brown-boardwalk-near-body-of-water-during-daytime-161029/",
+    "profile_photo": "https://www.pexels.com/photo/close-up-photography-of-a-man-wearing-bonnet-1054685/",
+    "description": "Lorem ipsum dolor sit amet, no porro tincidunt sit, malis nulla est ne. Per ad velit assentior, ex per laudem utamur dissentiet. Aliquid definiebas nec ne, ne sea congue iisque impedit. Eleifend accommodare interpretaris vix ei. Et usu simul tibique sententiae, antiopam euripidis constituto per et, quo sint denique accusamus ex."
+  },
+  {
+    "name": "Ronald McDonald",
+    "email": "scaryclown@yahoo.com",
+    "location": "Oak Brook, IL",
+    "cover_photo": "https://www.pexels.com/photo/bloom-blooming-blur-brick-wall-68470/",
+    "profile_photo": "https://www.pexels.com/photo/close-up-photography-of-a-man-1081188/",
+    "description": "Lorem ipsum dolor sit amet, no porro tincidunt sit, malis nulla est ne. Per ad velit assentior, ex per laudem utamur dissentiet. Aliquid definiebas nec ne, ne sea congue iisque impedit. Eleifend accommodare interpretaris vix ei. Et usu simul tibique sententiae, antiopam euripidis constituto per et, quo sint denique accusamus ex."
+  },
+  {
+    "name": "Cody Bot",
+    "email": "idontmoonomore@yahoo.com",
+    "location": "Los Angeles, CA",
+    "cover_photo": "https://www.pexels.com/photo/gray-stone-on-grassy-field-during-under-cloudy-sky-during-sunset-167976/",
+    "profile_photo": "https://www.pexels.com/photo/beard-boy-close-up-eyes-596887/",
+    "description": "Lorem ipsum dolor sit amet, no porro tincidunt sit, malis nulla est ne. Per ad velit assentior, ex per laudem utamur dissentiet. Aliquid definiebas nec ne, ne sea congue iisque impedit. Eleifend accommodare interpretaris vix ei. Et usu simul tibique sententiae, antiopam euripidis constituto per et, quo sint denique accusamus ex."
+  },
+  {
+    "name": "George Curious",
+    "email": "whatsthat@yahoo.com",
+    "location": "Miami, FL",
+    "cover_photo": "https://www.pexels.com/photo/bloom-blooming-blur-brick-wall-68470/",
+    "profile_photo": "https://www.pexels.com/photo/man-in-black-zip-up-hoodie-1080243/",
+    "description": "Lorem ipsum dolor sit amet, no porro tincidunt sit, malis nulla est ne. Per ad velit assentior, ex per laudem utamur dissentiet. Aliquid definiebas nec ne, ne sea congue iisque impedit. Eleifend accommodare interpretaris vix ei. Et usu simul tibique sententiae, antiopam euripidis constituto per et, quo sint denique accusamus ex."
+ }
+]
